ebb51b36724e3b96bbd936583a782bf590cd66bd
cyu17 <cyu17@illinois.edu>
2019-10-06 00:01:49 -0500

    sprintf need a constant size

--- mp2/node.cpp
+++ mp2/node.cpp
@@ -255,20 +255,20 @@ int join(){	//send JOIN to introducer, receie NEIGHBORS
     recv_info[valread] = '\0';
 	printf("\nThe info received is: %s\n", recv_info); //neighbor list
 
-	char delim[] = " ";
-	char *ptr = strtok(recv_info, delim);
-	for(int i=0; i++; i<NUM_NBR){
-		//printf("'%s'\n", ptr); 
-		try{
+	try{
+		char delim[] = " ";
+		char *ptr = strtok(recv_info, delim);
+		for(int i=0; i++; i<NUM_NBR){
+			//printf("'%s'\n", ptr); 
 			int nth = stoi(strtok(NULL, delim));
 			neighbors[nth].id = stoi(strtok(NULL, delim));
 			neighbors[nth].status = stoi(strtok(NULL, delim));
 			neighbors[nth].addr = (string) strtok(NULL, delim);
-        } catch (...){
-            cout<<"recv_info format is incorrect\n";
-            throw;
-        }
-	}
+		}
+	} catch (...){
+        cout<<"recv_info format is incorrect\n";
+        throw;
+    }
 	return 0;
 }
 
@@ -331,7 +331,7 @@ int test(){
 		if (strcmp(received_info,"LEAVE")==0){
 			leave();
 			myinfo.status = 0;
-			close(introducer.sock);
+			//close(introducer.sock);
 			msg = "OK";
 		}
 		if (strcmp(received_info,"INFO")==0){
@@ -413,8 +413,6 @@ int main(int argc, char const *argv[]) {
 	while(true){
 		sleep(heartbeat_time/1000);
 		if (myinfo.status==1){
-			try{
-
 			bool is_changed = true;
 		    for (int i=0;i<NUM_NBR;i++){ 
 				cur_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
@@ -423,7 +421,7 @@ int main(int argc, char const *argv[]) {
 					if (neighbors[i].status == 1){
 						neighbors[i].status = 0;
 						//string cmd = "LEAVE_"+id+"_"+i;
-						char *tmp;
+						char tmp[32] = {};
 						sprintf(tmp,"FAIL_%d_%d",myinfo.id,neighbors[i].id);
 					    send(introducer.sock, (const char *)tmp, strlen(tmp), 0);
 					    printf("cmd sent %s \n ", cmd.c_str());
@@ -444,13 +442,6 @@ int main(int argc, char const *argv[]) {
 				myfile << i <<" "<<neighbors[i].id<<" "<<neighbors[i].status<<"\n";
 			}
 			myfile.close();
-
-			}catch (...){
-	            //close(introducer.sock);
-	            printf("something wrong");
-	            myinfo.status=0;
-	            throw;
-	        }
 		}
 	}
 	close(introducer.sock);
