e76fdaed4e4f4b45fd2eb714fce5ca5fcdf89881
llyvonne9 <llyvonne9gd@gmail.com>
2019-12-07 23:24:55 -0600

    add files to deploy

--- mp4/deploy.py
+++ mp4/deploy.py
@@ -7,7 +7,7 @@ for i in range(1, 11):
     #os.system('ssh -t %s "sudo mkdir /home/deploy/mp2; sudo chgrp csvm425-cls /home/deploy/mp2; sudo chmod -R g+rw /home/deploy/mp2; sudo pkill -f \"node\""' %(server))
     os.system('ssh -t %s "sudo mkdir /home/deploy/mp4;sudo chgrp csvm425-cls /home/deploy/mp4; sudo chmod -R g+rw /home/deploy/mp4; sudo pkill -f \"node\""' %(server))
 
-    os.system('scp node.cpp test.cpp introducer.cpp maple_juice.h run.sh %s:/home/deploy/mp4' %(server))
+    os.system('scp node.cpp test.cpp introducer.cpp maple_juice.h run.sh wordcount.h test.txt %s:/home/deploy/mp4' %(server))
     os.system('scp serverss.txt %s:/home/deploy/mp4/servers.txt' %(server))
     os.system('ssh %s "cd /home/deploy/mp4; ./run.sh"' %(server))
     # subprocess.Popen(['ssh', server, "sh /home/deploy/mp2/run.sh"]) 
